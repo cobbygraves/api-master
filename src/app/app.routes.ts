2496cfb485425e9341cc7d66f3b0e325cdc4bda2
cobbygraves <rmeocnis@hotmail.com>
2025-06-18 10:28:54 +0000

    add post service

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { PostsComponent } from './components/posts/posts.component';
+import { ShowPostComponent } from './components/show-post/show-post.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'home',
+    component: PostsComponent,
+  },
+
+  {
+    path: 'post/:id',
+    component: ShowPostComponent,
+  },
+];
---
+++ src/app/components/show-post/show-post.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { PostServiceService } from '../../services/post-service.service';
+import { Post } from '../../models/post';
+import { ActivatedRoute } from '@angular/router';
+import { Comment } from '../../models/comment';
+
+@Component({
+  selector: 'app-show-post',
+  imports: [],
+  templateUrl: './show-post.component.html',
+  styleUrl: './show-post.component.css',
+})
+export class ShowPostComponent implements OnInit {
+  singlePost: Post | null = null;
+  postComments: Comment[] | null = null;
+
+  constructor(
+    private postService: PostServiceService,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    const postId = this.route.snapshot.paramMap.get('id')!;
+    this.postService.getSinglePost(parseInt(postId)).subscribe({
+      next: (value: Post) => {
+        this.singlePost = value;
+      },
+    });
+
+    this.postService.getPostComments(postId).subscribe({
+      next: (value) => {
+        this.postComments = value;
+      },
+    });
+  }
+}
